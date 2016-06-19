3333a63293f153e9c955e350ca04deb9b00c824d
nwallen <nicholas.a.wallen@gmail.com>
2016-06-19 11:53:03 -0700

    Milestone 4

--- facebookPhoto/PhotoTransition.swift
+++ facebookPhoto/PhotoTransition.swift
@@ -32,8 +32,9 @@ class PhotoTransition: BaseTransition {
         toViewController.view.alpha = 0
         UIView.animateWithDuration(duration, animations: {
             self.tempImageView.frame = toViewController.view.convertRect(toViewController.photoView.frame, fromCoordinateSpace: toViewController.photoView.superview!)
+            
         }) { (finished: Bool) -> Void in
-            UIView.animateWithDuration(self.duration, animations: {
+            UIView.animateWithDuration(0.2, animations: {
                 toViewController.view.alpha = 1
                 }) { (finished: Bool) -> Void in
                     self.tempImageView.removeFromSuperview()
@@ -45,11 +46,33 @@ class PhotoTransition: BaseTransition {
     
     override func dismissTransition(containerView: UIView, fromViewController: UIViewController, toViewController: UIViewController) {
         
-        fromViewController.view.alpha = 1
-        UIView.animateWithDuration(duration, animations: {
+        let tabViewController = toViewController as! UITabBarController
+        let navigationController = tabViewController.selectedViewController as! UINavigationController
+        let feedViewController = navigationController.topViewController as! FeedViewController
+        let fromViewController = fromViewController as! PhotoViewController
+        
+        
+        tempImageView = UIImageView()
+        tempImageView.contentMode = UIViewContentMode.ScaleAspectFit
+        tempImageView.image = selectedPhotoView.image
+        feedViewController.view.addSubview(tempImageView)
+        
+        tempImageView.frame = fromViewController.view.convertRect(fromViewController.photoView.frame, fromCoordinateSpace: fromViewController.photoView.superview!)
+        
+       
+        UIView.animateWithDuration(0.1, animations: {
             fromViewController.view.alpha = 0
-        }) { (finished: Bool) -> Void in
-            self.finish()
+          
+            }) { (finished: Bool) -> Void in
+                UIView.animateWithDuration(self.duration, animations: {
+                    self.tempImageView.frame = feedViewController.view.convertRect( self.selectedPhotoView.frame, fromCoordinateSpace: self.selectedPhotoView.superview!)
+                    self.tempImageView.alpha = 0
+                    }) { (finished: Bool) -> Void in
+                        self.tempImageView.removeFromSuperview()
+                        self.finish()
+                        
+                }
         }
+     
     }
 }
